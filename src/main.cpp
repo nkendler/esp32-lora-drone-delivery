0e5c8bac28777a1fcd3933f0f393e8269ade24f0
Nicolas Kendler <nicolas.kendler@gmail.com>
2022-01-13 20:01:36 -0500

    ben did something

--- src/main.cpp
+++ src/main.cpp
@@ -8,31 +8,36 @@
   this project also realess in GitHub:
   https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series
 */
-#include "heltec.h"
 #include "Arduino.h"
+#include "heltec.h"
 
 //#include "heltecv2.h"
 #define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
 
 int counter = 0;
 
-//SSD1306Wire * display;
-
 void setup() {
-  
+  //heltec = new Heltec_ESP32();
   //WIFI Kit series V1 not support Vext control
   Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+  Serial.println(int(Heltec.display));
+  Heltec.DisplayText("First Line of Text");
+  Serial.println("Cleared Display");
+  // Heltec.display->flipScreenVertically();  
+  // Heltec.display->setFont(ArialMT_Plain_10);
+  // Heltec.display->setContrast(255);
+	// delay(300);
 }
 
 void loop() {
   Serial.println("Sending packet: ");
   //Serial.println(counter);
-  /*Heltec.display->clear();
-	Heltec.display->drawString(2, 0, "Sending packet: ");
-	Heltec.display->display();*/
+  // Heltec.display->clear();
+	// Heltec.display->drawString(0, 0, "Sending packet: ");
+	// Heltec.display->display();
 	delay(300);
   // send packet
-  LoRa.beginPacket();
+  //LoRa.beginPacket();
 /*
 * LoRa.setTxPower(txPower,RFOUT_pin);
 * txPower -- 0 ~ 20
