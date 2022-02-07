0952ac8acae37d748a5b69c8a4b3781f81136e22
zbotpoint <zachary.lapointe@gmail.com>
2022-02-07 13:59:47 -0500

    Adding drone class and file

---
+++ lora-network/drone.cpp
@@ -0,0 +1,23 @@
+/* Code for the drone station
+*/
+#define ESP32 1
+#define DEBUG 1
+
+#include "Arduino.h"
+#include "heltec.h"
+#include "ECE496.h"
+#include <arduino_lmic.h>
+
+#define BAND 915E6
+
+class Drone;
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
