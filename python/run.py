c9bb48b2abf9640d0c5cf09d4b03bb9f69369ac9
ben-morehead <ben.morehead1221@gmail.com>
2022-02-07 14:08:57 -0500

    Run update

--- python/run.py
+++ python/run.py
@@ -1,2 +1,33 @@
+import argparse
+import pandas as pd
+import numpy as np
+import time
+
+class SheetParser():
+    def __init__(self, sheet_path=None):
+        self.sheet_df = None
+        if sheet_path is not None:
+            self.import_sheet(sheet_path)
+
+    def import_sheet(self, sheet_path):
+        try:
+            self.sheet_df = pd.read_excel(sheet_path)
+            print(f"Spreadsheet Succesfully Loaded: {time.time()}")
+        except:
+            print(f"No Spreadsheet at Location: {sheet_path}")
+
+    def display_data(self):
+        if self.sheet_df is not None:
+            print(self.sheet_df)
+        else:
+            print("NO SPREADSHEET LOADED")
+
+
 if __name__ == "__main__":
-    print("Run.py - But on the CLI Branch")
+
+    sheet_path= "order.xls"
+
+    #Going to turn into an argument
+    sp = SheetParser(sheet_path)
+
+    sp.display_data()
