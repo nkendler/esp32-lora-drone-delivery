e492606f2d627e6a27f45b743de079ce99eb3983
ben-morehead <ben.morehead1221@gmail.com>
2022-03-06 16:32:58 -0500

    Changing up the order_receiver to export the packet info

--- python/order_receiver.py
+++ python/order_receiver.py
@@ -2,6 +2,7 @@ import argparse
 import pandas as pd
 import numpy as np
 import time, os, sys, serial
+from datetime import datetime
 from serial import Serial
 import serial.tools.list_ports as ports
 
@@ -21,13 +22,21 @@ class OrderReceiver():
     """
     def __init__(self): 
         print("Initializing Order Receiver")
+        self.export_path = "order_exports"
         self.packet_list = []
+    
+    def _get_order_name(self):
+        order_name = datetime.now().strftime("droneorder_%Y%m%d%H%M%S.xlsx")
+        return order_name
     
     def get_packet_from_serial(self):
         sample_packet = [0x02, 0x80, 0x45, 0x18, 0x44]
         self.packet_list.append(sample_packet)
         #FINAL PACKET SHOULD BE 0x0280451844
         
+    def export_dataframe(self, dataframe):
+        order_name = self._get_order_name()
+        dataframe.to_excel("{}\\{}".format(self.export_path, order_name))  
 
     def decode_order_line(self, order_line):
         #print(len(order_line))
@@ -37,17 +46,35 @@ class OrderReceiver():
                 new_value = new_shift
             else:
                 new_value = new_value | new_shift    
-        return new_value
+        
+        order = new_value
 
+        df_list = []
+        for i in range(0, 7):
+            if i == 0:
+                df_list.append(order & 0x3f)
+                order = order >> 6
+            else:
+                df_list.append(order & 0x1f)
+                if i != 6: order = order >> 5
+        
+        return df_list
+        
 
     def format_packet_into_df(self):
+        df = None
         self.get_packet_from_serial()
         while len(self.packet_list) > 0:
             order_line = self.packet_list.pop(0)
             df_row = self.decode_order_line(order_line)
-            print(hex(df_row))
+            if df is None: 
+                df = pd.DataFrame([df_row], columns=["Rural Station", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"])
+            else:
+                df.loc[len(df)] = df_row
+        return df
 
 if __name__ == "__main__":
     odr = OrderReceiver()
-    odr.format_packet_into_df()
+    df = odr.format_packet_into_df()
+    odr.export_dataframe(df)
 
