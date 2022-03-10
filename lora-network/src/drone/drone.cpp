c2794a0f9dab1ae7f5da78f4b0ed38bc2cd9c18f
zbotpoint <zachary.lapointe@gmail.com>
2022-03-10 01:16:31 -0500

    Adding code to drone FSM to communicate with hospital

--- lora-network/src/drone/drone.cpp
+++ lora-network/src/drone/drone.cpp
@@ -22,20 +22,44 @@ uint8_t s_packet_buf[PACKET_SIZE];
 
 namespace ECE496 {
 class Drone {
-    uint8_t orders_buf[MAX_ORDERS][PACKET_SIZE] = {{NULL}};
+    uint8_t orders_buf[MAX_ORDERS][PACKET_SIZE] = {{0}};
     int num_orders = 0;
 
-   public:
+    public:
     int addOrder(uint8_t* order) {
         if (num_orders < MAX_ORDERS) {
             memcpy(orders_buf[num_orders], order, PACKET_SIZE);
+            orders_buf[num_orders][0] & 0x0F;  // clear station and packet type info
             num_orders++;
             return 1;
-        } else
+        } else {
             return 0;
+        }
     };
+    int getOrder(uint8_t* order) {
+        if (num_orders > 0) {
+            memcpy(order, orders_buf[num_orders], PACKET_SIZE);
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    };
+    int popOrder() {
+        if (num_orders > 0) {
+            memset(orders_buf[num_orders], 0, PACKET_SIZE);
+            num_orders--;
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
     bool hasSpace() {
         return num_orders < MAX_ORDERS;
+    }
+    bool hasOrder() {
+        return num_orders > 0;
     }
     enum State {
         WAIT = 0,
@@ -46,6 +70,8 @@ class Drone {
         RESPOND,
         GROUND_EXCHANGE,
         GROUND_IV,
+        HOSPITAL_EXCHANGE,
+        HOSPITAL_IV,
         READY,
         ERROR,
         CLOSE
@@ -124,13 +150,25 @@ void loop() {
         case ECE496::Drone::VERIFY: {
             if (ECE496::Utils::getPacketStationType(r_packet_buf) == ECE496::Utils::HOSPITAL && ECE496::Utils::getPacketType(r_packet_buf) == ECE496::Utils::ACK) {
                 ECE496::Utils::displayTextAndScroll("ACK detected from hospital");
-                NextState = ECE496::Drone::CLEAR;
+                NextState = ECE496::Drone::UPLOAD;
+
             } else if (ECE496::Utils::getPacketStationType(r_packet_buf) == ECE496::Utils::HOSPITAL && ECE496::Utils::getPacketType(r_packet_buf) == ECE496::Utils::HELLO) {
                 ECE496::Utils::displayTextAndScroll("HELLO detected from hospital");
-                NextState = ECE496::Drone::UPLOAD;
+                
+                // check if we have an order to upload
+                if (Drone->hasOrder()) {
+                    ECE496::Utils::buildPacket(s_packet_buf, ECE496::Utils::ACK, PACKET_SIZE, NULL);
+                    ECE496::Utils::sendUnencryptedPacket(s_packet_buf, PACKET_SIZE);
+                    NextState = ECE496::Drone::HOSPITAL_EXCHANGE;
+                } else {  // otherwise ignore the message
+                    ECE496::Utils::displayTextAndScroll("No order to deliver.");
+                    NextState = ECE496::Drone::CLOSE;
+                }
+                
             } else if (ECE496::Utils::getPacketStationType(r_packet_buf) == ECE496::Utils::GROUND && ECE496::Utils::getPacketType(r_packet_buf) == ECE496::Utils::PAYLOAD) {
                 ECE496::Utils::displayTextAndScroll("Packet detected from ground station.");
                 NextState = ECE496::Drone::STORE;
+
             } else if (ECE496::Utils::getPacketStationType(r_packet_buf) == ECE496::Utils::GROUND && ECE496::Utils::getPacketType(r_packet_buf) == ECE496::Utils::HELLO) {
                 ECE496::Utils::displayTextAndScroll("Hello detected from ground station.");
 
@@ -180,6 +218,66 @@ void loop() {
                 NextState = ECE496::Drone::WAIT;
             }
 
+            break;
+        }
+
+        // Exchange public keys with the hospital station
+        case ECE496::Drone::HOSPITAL_EXCHANGE: {
+            ECE496::Utils::generateKeys();
+            if (ECE496::Utils::awaitPacketUntil(PACKET_WAIT_TIME)) {
+                ECE496::Utils::receiveUnencryptedPacket(ECE496::Utils::f_publicKey, KEY_SIZE);
+                ECE496::Utils::sendUnencryptedPacket(ECE496::Utils::publicKey, KEY_SIZE);
+                NextState = ECE496::Drone::HOSPITAL_IV;
+            } else {
+                NextState = ECE496::Drone::WAIT;
+            }
+            break;
+        }
+                                        
+        // Receive the IV for encryption from the hospital
+        case ECE496::Drone::HOSPITAL_IV: {
+            if (ECE496::Utils::awaitPacketUntil(PACKET_WAIT_TIME)) {
+                ECE496::Utils::receiveUnencryptedPacket(ECE496::Utils::IV, IV_SIZE);
+                ECE496::Utils::buildPacket(s_packet_buf, ECE496::Utils::ACK, PACKET_SIZE, NULL);
+                ECE496::Utils::sendUnencryptedPacket(s_packet_buf, PACKET_SIZE);
+
+                // encrypt
+                ECE496::Utils::generateSecret();
+                ECE496::Utils::chacha.setKey(ECE496::Utils::sharedKey, KEY_SIZE);
+                ECE496::Utils::chacha.setIV(ECE496::Utils::IV, IV_SIZE);
+                NextState = ECE496::Drone::READY;
+            } else {
+                NextState = ECE496::Drone::WAIT;
+            }
+
+            break;
+        }
+
+        // Sends an encypted payload packet to the hospital station
+        case ECE496::Drone::UPLOAD: {
+            if (Drone->getOrder(s_packet_buf)) {
+                ECE496::Utils::buildPacket(s_packet_buf, ECE496::Utils::PAYLOAD, PACKET_SIZE, s_packet_buf);
+                ECE496::Utils::sendPacket(s_packet_buf, PACKET_SIZE);
+
+                // wait for ACK from hospital
+                if (ECE496::Utils::awaitPacketUntil(PACKET_WAIT_TIME)) {
+                    ECE496::Utils::receivePacket(r_packet_buf, PACKET_SIZE);
+
+                    if (ECE496::Utils::getPacketStationType(r_packet_buf) == ECE496::Utils::HOSPITAL && ECE496::Utils::getPacketType(r_packet_buf) == ECE496::Utils::ACK) {
+                        Drone->popOrder(); // remove order if it arrived ok
+                        NextState = ECE496::Drone::UPLOAD; // try to send another payload
+                    }
+                    else {
+                        NextState = ECE496::Drone::CLOSE;
+                    }
+                } else {
+                    NextState = ECE496::Drone::CLOSE;
+                }
+            }
+            else {
+                // exit secure channel if we have no more orders to upload
+                NextState = ECE496::Drone::CLOSE;
+            }
             break;
         }
 
